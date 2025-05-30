9b531cf491fcdf5b9afcfe6dd73b2f70e705f418
bokjuls <sanchojulia017@gmail.com>
2025-05-30 21:19:56 +0800

    Delete QR system directory

--- QR system/Database/delete.sql
+++
@@ -1,3 +0,0 @@
-/* Deletion of Data, if required */
-DELETE FROM ATTENDANCE WHERE attendance_id = 1; /* or any specific condition */
-DELETE FROM STUDENT WHERE student_id = 1; /* if really needed */
